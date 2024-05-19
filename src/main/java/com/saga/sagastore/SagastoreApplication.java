93e910aa9979606ee9f39623c8531dc4597d5c9c
bayuramdhan50 <bayuramdhan50@gmail.com>
2024-05-19 11:05:52 +0700

    komponent nya

---
+++ src/main/java/com/saga/sagastore/SagastoreApplication.java
@@ -0,0 +1,13 @@
+package com.saga.sagastore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SagastoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SagastoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/saga/sagastore/SagastoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saga.sagastore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SagastoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
