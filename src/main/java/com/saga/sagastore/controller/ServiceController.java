b72193b3e20d0e0844cbbe0dcfc4d73fe6841996
bayuramdhan50 <bayuramdhan50@gmail.com>
2024-05-20 14:44:30 +0700

    <keranjang, signup dan servie> beres

---
+++ src/main/java/com/saga/sagastore/controller/ServiceController.java
@@ -0,0 +1,20 @@
+package com.saga.sagastore.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class ServiceController {
+
+    @GetMapping("/services")
+    public String services(Model model) {
+        model.addAttribute("pageTitle", "Services");
+        model.addAttribute("active", "services");
+        return "services";
+    }
+}
