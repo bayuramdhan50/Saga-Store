c347ac44f0981a80b26479bcfb3584ef76e04b74
bayuramdhan50 <bayuramdhan50@gmail.com>
2024-05-20 13:17:34 +0700

    <about> about beres

---
+++ src/main/java/com/saga/sagastore/controller/AboutController.java
@@ -0,0 +1,17 @@
+package com.saga.sagastore.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class AboutController {
+    @GetMapping("/about")
+    public String about(Model model) {
+        return "about";
+    }
+}
